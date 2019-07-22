db4e542cc39b1bd8425e70e36d9b5c03696a6154
Suresh Peiris <tsmpmail@gmail.com>
2019-07-23 01:02:47 +0530

    Added colombo

---
+++ colombo.php
@@ -0,0 +1,49 @@
+<?php include('includes/base/header.php') ?>
+<style>
+body {
+    background-image: url(img/colomboBG.png);
+}
+</style>
+<div class="homeFeatured">
+   <h1>Explore Colombo</h1>
+   <br>
+   <p class="subtitle">Colombo, the capital of Sri Lanka, has a long history as a port on ancient east-west trade routes, ruled successively by the Portuguese, Dutch and British. That heritage is reflected in its its architecture, mixing colonial buildings with high-rises and shopping malls. The imposing Colombo National Museum, dedicated to Sri Lankan history, borders sprawling Viharamahadevi Park and its giant Buddha.
+</p>
+</div>
+
+<center><h2 style="margin-bottom:0;">ATTRACTIONS</h2></center>
+<div class="wrapper">
+   <div class="container">
+      <div class="block" id="blkGalleface">
+         <h2>Galle Face Green</h2>
+         <p>Galle Face is a 5 ha (12 acres) ocean-side urban park, which stretches for 500 m (1,600 ft) along the coast, in the heart of Colombo, the financial and business capital of Sri Lanka. The promenade was initially laid out in 1859 by Governor Sir Henry George Ward, although the original Galle Face Green extended over a much larger area than is seen today. </p>
+         <a class="btn" href="https://goo.gl/maps/t2qH84APR6Y4etmi9">Visit here</a>
+      </div>
+      <div class="block" id="blkGangarama">
+         <h2>Gangaramaya Temple</h2>
+         <p>The temple's architecture demonstrates an eclectic mix of Sri Lankan, Thai, Indian, and Chinese architecture. This Buddhist temple includes several imposing buildings and is situated not far from the placid waters of Beira Lake on a plot of land that was originally a small hermitage on a piece of marshy land.</p>
+         <a class="btn" href="https://goo.gl/maps/iP1kd9Q2pzL2Xs3RA">Visit here</a>
+      </div>
+      <div class="block" id="blkMuseum">
+         <h2>National Museum of Colombo</h2>
+         <p>National Museum of Colombo, also known as the Sri Lanka National Museum is one of two museums in Colombo. It is the largest museum in Sri Lanka. It is maintained by the Department of National Museum of the central government. The museum holds contains a collections of much importance to Sri Lanka such as the regalia of the country</p>
+         <a class="btn" href="https://goo.gl/maps/zitVxYARv3NJrYZg6">Visit here</a>
+      </div>
+      <div class="block" id="blkViharamahadevi">
+         <h2>Viharamahadevi Park</h2>
+         <p>Viharamahadevi Park is a public park located in Colombo, next to the National Museum in Sri Lanka. It is the oldest and largest park of the Port of Colombo. Situated in front of the colonial-era Town Hall building, the park is named after Queen Viharamahadevi, the mother of King Dutugamunu. </p>
+         <a class="btn" href="https://goo.gl/maps/6LjAUB4FZJVnNkk76">Visit here</a>
+      </div>
+      <div class="clearfix"></div>
+      <a id="shop"></a>
+      <div class="bigBlock">
+         <h1>
+         Travel Sri Lanka Store
+         <h1>
+         <p style="width:300px;">Want to purchase amazing Sri Lankan ornaments? Visit our online store and purchase as you wish. Lowest price in Island.</p>
+         <a class="btn" href="shop.php">Shop Online</a>   
+      </div>
+   </div>
+</div>
+
+<?php include('includes/base/footer.php') ?>
