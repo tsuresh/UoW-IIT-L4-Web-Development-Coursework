619bb21aba2350b4afa21b5c9977bda811c6cea7
Suresh Peiris <tsmpmail@gmail.com>
2019-07-23 11:53:50 +0530

    Added missing meta tags

--- includes/base/header.php
+++ includes/base/header.php
@@ -3,6 +3,9 @@
    <head>
       <meta name="language" content="English">
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+      <meta name="author" content="Suresh Peiris">
+      <meta name="keywords" content="travel,sri lanka,colombo,sri lankan,sri lanka tourism,nuwara eliya,kandy,galle,places to visit">
+      <meta name="description" content="Travel Sri Lanka official website">
       <title>Travel Sri Lanka</title>
       <link href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700&display=swap" rel="stylesheet">
       <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
