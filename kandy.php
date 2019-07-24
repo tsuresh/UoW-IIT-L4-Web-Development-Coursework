d5a17aa06edc2cc673b41fa997d450e036fb3712
Suresh Peiris <tsmpmail@gmail.com>
2019-07-24 07:05:16 +0530

    Added Kandy page

---
+++ kandy.php
@@ -0,0 +1,49 @@
+<?php $title = "Kandy"; include('includes/base/header.php') ?>
+<style>
+body {
+    background-image: url(img/kandy-lake.png);
+}
+</style>
+<div class="homeFeatured">
+   <h1>Explore Knady</h1>
+   <br>
+   <p class="subtitle">Kandy is a large city in central Sri Lanka. It's set on a plateau surrounded by mountains, which are home to tea plantations and biodiverse rainforest. The city's heart is scenic Kandy Lake (Bogambara Lake), which is popular for strolling. Kandy is famed for sacred Buddhist sites, including the Temple of the Tooth (Sri Dalada Maligawa) shrine, celebrated with the grand Esala Perahera annual procession.
+</p>
+</div>
+
+<center><h2 style="margin-bottom:0;">ATTRACTIONS</h2></center>
+<div class="wrapper">
+   <div class="container">
+      <div class="block" id="blkTemple of the Sacred Tooth Relic">
+         <h2>Temple of the Sacred Tooth Relic</h2>
+         <p>Sri Dalada Maligawa or the Temple of the Sacred Tooth Relic is a Buddhist temple in the city of Kandy, Sri Lanka. It is located in the royal palace complex of the former Kingdom of Kandy, which houses the relic of the tooth of the Buddha. Since ancient times, the relic has played an important role in local politics because it is believed that whoever holds the relic holds the governance of the country. Kandy was the last capital of the Sri Lankan kings and is a World Heritage Site mainly due to the temple.</p>
+         <a class="btn" href="https://goo.gl/maps/t2qH84APR6Y4etmi9">Visit here</a>
+      </div>
+      <div class="block" id="blkRoyal Botanical Gardens, Peradeniya">
+         <h2>Royal Botanical Gardens, Peradeniya</h2>
+         <p>Royal Botanic Gardens, Peradeniya are about 5.5 km to the west of the city of Kandy in the Central Province of Sri Lanka. It attracts 2 million visitors annually. It is near the Mahaweli River. It is renowned for its collection of orchids.</p>
+         <a class="btn" href="https://goo.gl/maps/iP1kd9Q2pzL2Xs3RA">Visit here</a>
+      </div>
+      <div class="block" id="blkKandy Lake">
+         <h2>Kandy Lake</h2>
+         <p>Kandy Lake, also known as Kiri Muhuda or the Sea of Milk, is an artificial lake in the heart of the hill city of Kandy, Sri Lanka, built in 1807 by King Sri Wickrama Rajasinghe next to the Temple of the Tooth. Over the years, it was reduced in size. It is a protected lake, with fishing banned.</p>
+         <a class="btn" href="https://goo.gl/maps/zitVxYARv3NJrYZg6">Visit here</a>
+      </div>
+      <div class="block" id="blkNational Museum of Kandy">
+         <h2>National Museum of Kandy</h2>
+         <p>The National Museum of Kandy in Kandy, Sri Lanka is located next to the Temple of the Tooth in part of the former Royal Palace of Kandy. The primary exhibits are housed in the Palle Vahala building, which was the former home of the King's harem. A secondary exhibition is located in the main palace building. </p>
+         <a class="btn" href="https://goo.gl/maps/6LjAUB4FZJVnNkk76">Visit here</a>
+      </div>
+      <div class="clearfix"></div>
+      <a id="shop"></a>
+      <div class="bigBlock">
+         <h1>
+         Travel Sri Lanka Store
+         <h1>
+         <p style="width:300px;">Want to purchase amazing Sri Lankan ornaments? Visit our online store and purchase as you wish. Lowest price in Island.</p>
+         <a class="btn" href="shop.php">Shop Online</a>   
+      </div>
+   </div>
+</div>
+
+<?php include('includes/base/footer.php') ?>
